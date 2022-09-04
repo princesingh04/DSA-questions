b963af26e1caae8465ef93a24488d017ce528815
princesingh04 <111722098+princesingh04@users.noreply.github.com>
2022-09-04 09:21:11 +0530

    Add files via upload

---
+++ GFG/parenthsischcker.cpp
@@ -0,0 +1,18 @@
+    bool ispar(string x)
+    {
+        // Your code here
+        stack<char> st;
+        for(int i =0; i< x.length(); i++){
+            if(st.empty()){
+                st.push(x[i]);
+            }else if((st.top()=='(' && x[i]==')') || (st.top()=='{' && x[i]=='}') || (st.top()=='[' && x[i]==']')){
+                st.pop();
+            }else{
+                st.push(x[i]);
+            }
+        }if(st.empty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
